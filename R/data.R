dc1b5ff0c09a31fe7fc9b1c4396f75751a3a285b
gallaghershk <shannon.gallagher@nih.gov>
2020-01-17 12:28:36 -0500

    added Williams data

--- R/data.R
+++ R/data.R
@@ -7,7 +7,7 @@
 #' \describe{
 #' \item{well}{ one of iRBC (the max), uRBC (the min), RPMI (??), or comb (which is short for combination)}
 #' \item{AMA1RON2}{dose of AMA1RON2 in mg/mL}
-#' \item{RH5}{dose of AMA1RON2 in mg/mL}
+#' \item{RH5}{dose of RH5 in mg/mL}
 #' \item{exp{x}{y}rep{z}}{the results from experiment x, sub item y, repetition z}
 #' }
 #' @examples
@@ -22,11 +22,27 @@
 #' @format a 38 x 15 data set where the columns are of the following format
 #' \describe{
 #' \item{well}{ one of iRBC (the max), uRBC (the min), RPMI (??), or comb (which is short for combination)}
-#' \item{RIPR}{dose of AMA1RON2 in mg/mL}
-#' \item{CyRPA}{dose of AMA1RON2 in mg/mL}
+#' \item{RIPR}{dose of RIPR in mg/mL}
+#' \item{CyRPA}{dose of CyRPA in mg/mL}
 #' \item{exp{x}{y}rep{z}}{the results from experiment x, sub item y, repetition z}
 #' }
 #' @examples
 #' data("cyrpa_ripr")
 #' head(cyrpa_ripr)
 "cyrpa_ripr"
+
+
+
+#' RH5 and RH4
+
+#' @description The data is the raw data for a combination dose of RH5 and RH4.  The data was collected by PEOPLE and on DATE on this GRANT.
+#' @format a 48 x 3 data set where the columns are of the following format
+#' \describe{
+#' \item{RH4}{dose of RH4 in mg/mL}
+#' \item{RH5}{dose of RH5 in mg/mL}
+#' \item{GIA}{Percent Growth inhibition assay averaged over two experiments}
+#' }
+#' @examples
+#' data("rh5_rh4")
+#' head(rh5_rh4)
+"rh5_rh4"
--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -7,3 +7,15 @@ load("data-raw/rh5_ama1ron2.rda")
 
 usethis::use_data(cyrpa_ripr, overwrite = TRUE)
 usethis::use_data(rh5_ama1ron2, overwrite = TRUE)
+
+
+
+rh5_rh4 <-read.csv("data-raw/Original GIA data for PLoS Pathog RH5 RH4.csv",
+                   header=TRUE, na.strings = c(""," ","  "))
+
+rh5_rh4 <- rh5_rh4 %>% dplyr::mutate(GIA = (GIA1 + GIA2) / 2) %>%
+  dplyr::rename(RH4 = AntiRh4.mg.mL,
+                RH5 = AntiRH5.mg.mL) %>%
+  dplyr::select(RH4, RH5, GIA)
+
+usethis::use_data(rh5_rh4, overwrite = TRUE)
